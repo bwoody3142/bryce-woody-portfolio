dbf92a9f4de55f57f90a7164d3662907916c60aa
bwoody3142 <bwoody3142@gmail.com>
2022-07-01 00:42:19 -0400

    Add tooltips

--- src/components/SideLinks.tsx
+++ src/components/SideLinks.tsx
@@ -5,16 +5,16 @@ import { AiOutlineMail, AiFillPhone, AiFillGithub, AiFillLinkedin } from 'react-
 const SideLinks = () => {
   return (<>
     <div className={styles.links}>
-        <a className="socialLink" href="mailto:bwoody3142@gmail.com">
+        <a className="has-tooltip" data-tooltip="Email Me @ bwoody3142@gmail.com" href="mailto:bwoody3142@gmail.com">
             <AiOutlineMail height="2em" width="2em" />
         </a>
-        <a className="socialLink" href="tel:15745280041">
+        <a className="has-tooltip" data-tooltip="Call or Text (574) 528 - 0041" href="tel:15745280041">
             <AiFillPhone />
         </a>
-        <a className="socialLink" href="https://github.com/bwoody3142">
+        <a className="has-tooltip" data-tooltip="GitHub" href="https://github.com/bwoody3142">
             <AiFillGithub />
         </a>
-        <a className="socialLink" href="https://www.linkedin.com/in/bryce-woody-2872481b4">
+        <a className="has-tooltip" data-tooltip="LinkedIn" href="https://www.linkedin.com/in/bryce-woody-2872481b4">
             <AiFillLinkedin />
         </a>
     </div></>
